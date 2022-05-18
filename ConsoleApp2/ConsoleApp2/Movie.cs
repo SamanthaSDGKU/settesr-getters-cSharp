a8cac826f4d9ad39465ed5457992e5f073e999ff
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-05-18 12:04:12 -0700

    movie project

---
+++ ConsoleApp2/ConsoleApp2/Movie.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp2
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating="";
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            this.title = aTitle;
+            this.director = aDirector;
+            Rating = aRating;
+        }
+
+        //method setter and getter
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie vida = new Movie("La vita e bella", "Roberto", "PG");
+            Movie devoluciones = new Movie("No se aceptan devoluciones", "Eugenio", "Dog");
+
+            //rating = PG,G,PG-13
+            
+            Console.WriteLine(devoluciones.Rating);
+            Console.ReadKey();
+
+        }
+    }
+}
